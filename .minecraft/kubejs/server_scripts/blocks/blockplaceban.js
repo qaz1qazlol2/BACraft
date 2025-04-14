47f10b1a7c03b4330099df6dc0c9aa4cb1449d73
qaz1qazlol2 <panyouchao1029@qaz1qazlol.onmicrosoft.com>
2025-04-15 00:28:32 +0800

    Ban Beacon Again
    1.重新禁止信标的破坏和放置

--- .minecraft/kubejs/server_scripts/blocks/blockplaceban.js
+++ .minecraft/kubejs/server_scripts/blocks/blockplaceban.js
@@ -1,5 +1,6 @@
 const banned_place_blocks = [            //禁止放置的方块
-    "twilightforest:uncrafting_table"    //拆解台
+    "twilightforest:uncrafting_table",   //拆解台
+    "minecraft:beacon"
 ]
 
 banned_place_blocks.forEach(banned_place_block =>{
